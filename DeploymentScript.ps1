69842eb4c18bb492b205f681855b5a318efaf41c
Toby Reid <tobyreid@users.noreply.github.com>
2022-10-14 17:34:33 +0100

    write debug

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -1,2 +1,5 @@
 Import-Module (Join-Path "." "EP.PowerShell.JiraDeployInfo")
+Write-Output $Env:JIRA_STATE
+Write-Output $Env:JIRA_ENVIRONMENT
+Write-Output $Env:JIRA_DOMAIN
 Update-AzureDeploymentInformation -State $Env:JIRA_STATE -EnvironmentType $Env:JIRA_ENVIRONMENT -JiraDomain $Env:JIRA_DOMAIN
